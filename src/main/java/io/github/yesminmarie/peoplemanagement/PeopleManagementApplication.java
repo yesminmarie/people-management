8444077907a1acb140c1e4b7f22f640459717bf2
Yesmin Lahoud <ymslahoud@gmail.com>
2023-01-02 17:53:35 -0300

    Initial commit

---
+++ src/main/java/io/github/yesminmarie/peoplemanagement/PeopleManagementApplication.java
@@ -0,0 +1,13 @@
+package io.github.yesminmarie.peoplemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleManagementApplication.class, args);
+	}
+
+}
